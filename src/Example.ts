39a6f4923f5ed75164d9713f8ada0609277aa491
James Chartrand <jc.chartrand@gmail.com>
2024-03-01 18:58:38 -0500

    Initial commit

---
+++ src/Example.ts
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2023 Digital Credentials Consortium. All rights reserved.
+ */
+export class Example {
+  public hello (): string {
+    return 'world'
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/*!
+ * Copyright (c) 2023 Digital Credentials Consortium. All rights reserved.
+ */
+export { Example } from './Example'
